e3104092b89a83b13b82d2538f7fa35a6e0200d3
Evgeny Daubel <mortuusars@gmail.com>
2021-07-29 03:45:23 +0300

    Change package for FolderPicker dialogs.

---
+++ PSQuickAssets/WPF/ViewManager.cs
@@ -0,0 +1,39 @@
+﻿using Ookii.Dialogs.Wpf;
+using PSQuickAssets.ViewModels;
+
+namespace PSQuickAssets.Infrastructure
+{
+    public class ViewManager
+    {
+        private MainView _mainView;
+        private MainViewModel _mainViewModel;
+
+        public void CreateAndShowMainView()
+        {
+            _mainViewModel = new MainViewModel(new ImageFileLoader());
+
+            _mainView ??= new MainView() { DataContext = _mainViewModel };
+            _mainView.Show();
+        }
+
+        public void CloseMainView()
+        {
+            _mainView?.Close();
+        }
+
+        public void ChangeWindowVisibility()
+        {
+            _mainViewModel.IsWindowShowing = !_mainViewModel.IsWindowShowing;
+        }
+
+        public static string ShowSelectDirectoryDialog()
+        {
+            var dialog = new VistaFolderBrowserDialog();
+
+            if (dialog.ShowDialog() is true)
+                return dialog.SelectedPath;
+            else
+                return string.Empty;
+        }
+    }
+}
