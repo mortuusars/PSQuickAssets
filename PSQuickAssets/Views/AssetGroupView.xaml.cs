a985fd62482029c91ca0af896903fac60ba1d7e6
Evgeny Daubel <mortuusars@gmail.com>
2021-12-12 10:39:28 +0200

    Group folding

--- PSQuickAssets/Views/AssetGroupView.xaml.cs
+++ PSQuickAssets/Views/AssetGroupView.xaml.cs
@@ -1,15 +1,47 @@
-﻿using System.Windows.Controls;
+﻿using PSQuickAssets.Controls;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
 
 namespace PSQuickAssets.Views
 {
-    /// <summary>
-    /// Interaction logic for FolderListBox.xaml
-    /// </summary>
     public partial class AssetGroupView : UserControl
     {
+        public static readonly DependencyProperty IsExpandedProperty =
+            DependencyProperty.Register(nameof(IsExpanded), typeof(bool), typeof(AssetGroupView), new PropertyMetadata(true, OnExpandedChanged));
+
+        public bool IsExpanded
+        {
+            get { return (bool)GetValue(IsExpandedProperty); }
+            set { SetValue(IsExpandedProperty, value); }
+        }
+
         public AssetGroupView()
         {
             InitializeComponent();
+        }
+
+        private void Fold_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            IsExpanded = !IsExpanded;
+        }
+
+        private void AnimateExpander()
+        {
+            int toAngle = IsExpanded ? 360 : 270;
+            DoubleAnimation rotateAnim = new DoubleAnimation(toAngle, new Duration(TimeSpan.FromMilliseconds(140)));
+            var storyboard = new Storyboard();
+            storyboard.Children.Add(rotateAnim);
+            Storyboard.SetTarget(rotateAnim, expandButton);
+            Storyboard.SetTargetProperty(rotateAnim, new PropertyPath("RenderTransform.Angle"));
+            expandButton.BeginStoryboard(storyboard);
+        }
+
+        private static void OnExpandedChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var assetGroupView = (AssetGroupView)d;
+            assetGroupView.AnimateExpander();
         }
     }
 }
