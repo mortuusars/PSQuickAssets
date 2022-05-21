340c2858165032b7a636562195664bf79a49e122
mortuusars <mortuusars@gmail.com>
2022-05-21 22:53:07 +0300

    Updated style of "No Assets" buttons.

---
+++ PSQuickAssets/Utils/SystemDialogs/FileFilters.cs
@@ -0,0 +1,16 @@
+﻿namespace PSQuickAssets.Utils;
+
+/// <summary>
+/// Holds filters for Select File Dialog.
+/// </summary>
+public static class FileFilters
+{
+    /// <summary>
+    /// Filter for allowed image formats.
+    /// </summary>
+    public static string Images { get; } = "Images (*.bmp; *.jpg; *.jpeg; *.gif; *.tiff; *.tif; *.psd; *.psb)|*.bmp; *.jpg; *.jpeg; *.gif; *.tiff; *.tif; *.psd; *.psb";
+    /// <summary>
+    /// Filter for all files.
+    /// </summary>
+    public static string AllFiles { get; } = "All files(*.*)|*.*";
+}
